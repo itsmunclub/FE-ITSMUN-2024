8970c42ed5ca9b20b4b7f8c46050ffc02fce88f2
Sarah Nurhasna <sarah2501.snk@gmail.com>
2024-08-08 16:17:31 +0700

    making hero home page (not done yet)

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 const Sidebar = () => {
   return (
-    <div className='fixed z-50 flex flex-col justify-between bg-MUN-englishViolet w-fit h-[95%] p-5 rounded-3xl m-5 '>
-      <div className='flex flex-col gap-5'>
+    <div className='fixed z-50 flex flex-col justify-between bg-MUN-englishViolet w-fit h-[95%] px-5 py-10 rounded-3xl m-5 '>
+      <div className='flex flex-col gap-5 items-center'>
         <Image
           src="/munLogo.svg"
           width={64}
@@ -14,7 +14,7 @@ const Sidebar = () => {
         />
         <button className='Hall-Of-Fame'>
           <Link href="/hall-of-fame">
-            <div className="group flex bg-MUN-sidebar-linen rounded-full w-16 h-16 justify-center items-center hover:bg-MUN-beigeBanner p-5">
+            <div className="group flex bg-MUN-sidebar-linen rounded-full w-14 h-14 justify-center items-center hover:bg-MUN-beigeBanner p-3">
               <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className=" group-hover:fill-MUN-bg-purple-dark" fillRule="evenodd" clipRule="evenodd" d="M16.8 0.128906H7.19981H4.79982V2.52889L2.39999 2.52889H0V4.92888V14.5288H2.39999L4.79982 14.5288V16.9288L10.7998 16.9288V19.3288H7.2001V21.7288L10.7998 21.7288H13.1998L16.8 21.7288V19.3288H13.1998V16.9288L19.1997 16.9288V14.5288L19.1999 14.5288L19.2 14.5288L21.5997 14.5288H23.9997V4.92888H24V2.52889H23.9997H21.5997H19.2V0.128906H19.1997H16.8ZM16.8 14.5288V4.92888V2.52889L7.19996 2.52889V4.92888H7.19981V14.5288H16.8ZM21.5997 4.92888V12.1288H19.2V4.92888H21.5997ZM4.79982 12.1288H2.39999V4.92888H4.79982V12.1288Z" fill="#53405F"/>
               </svg>
@@ -22,10 +22,10 @@ const Sidebar = () => {
           </Link>
         </button>
       </div>
-      <div className='flex flex-col gap-5'>
+      <div className='flex flex-col gap-5 items-center'>
         <button className='Home'>
           <Link href="/home">
-            <div className="group flex bg-MUN-sidebar-linen rounded-full w-16 h-16 justify-center items-center hover:bg-MUN-beigeBanner p-5">
+            <div className="group flex bg-MUN-sidebar-linen rounded-full w-14 h-14 justify-center items-center hover:bg-MUN-beigeBanner p-3">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className=" group-hover:fill-MUN-bg-purple-dark" fillRule="evenodd" clipRule="evenodd" d="M10.2856 0.5H13.7142V3.92857H10.2856V0.5ZM13.7142 21.0716H24V24.5001H0V21.0716H10.2856V14.2147H13.7142V21.0716ZM17.1429 3.92844H13.7144V7.35701H17.1429V3.92844ZM17.143 7.35734H20.5715V10.7853H23.9999V21.071H20.5713V10.7859H17.143V7.35734ZM6.85703 3.92844H10.2856V7.35701H6.85703V3.92844ZM6.85728 7.35734H3.42871V10.7859H6.85728V7.35734ZM0 10.7853H3.42857V21.071H0V10.7853Z" fill="#53405F"/>
             </svg>
@@ -34,7 +34,7 @@ const Sidebar = () => {
         </button>
         <button className='Blog'>
           <Link href="/blog">
-            <div className="group flex bg-MUN-sidebar-linen rounded-full w-16 h-16 justify-center items-center hover:bg-MUN-beigeBanner p-5">
+            <div className="group flex bg-MUN-sidebar-linen rounded-full w-14 h-14 justify-center items-center hover:bg-MUN-beigeBanner p-3">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className=" group-hover:fill-MUN-bg-purple-dark" fillRule="evenodd" clipRule="evenodd" d="M24 0.218262H24.0001V2.1819H24V17.8911H24.0001V19.8547H4.3637V19.8546V17.8911V2.1819V0.218262H6.54552H21.8181H24ZM21.8181 2.1819H6.54552V17.8911H21.8181V2.1819ZM8.72722 4.14545H19.6363V6.10909H8.72722V4.14545ZM19.6363 8.07287H8.72722V10.0365H19.6363V8.07287ZM8.72722 12.0001H16.3636V13.9637H8.72722V12.0001ZM19.6364 21.8183H2.18182V4.14545H0V21.8183V23.7818V23.7819H19.6364V21.8183Z" fill="#53405F"/>
               </svg>
@@ -43,7 +43,7 @@ const Sidebar = () => {
         </button>
         <button className='Event'>
           <Link href="/event">
-            <div className="group flex bg-MUN-sidebar-linen rounded-full w-16 h-16 justify-center items-center hover:bg-MUN-beigeBanner p-5">
+            <div className="group flex bg-MUN-sidebar-linen rounded-full w-14 h-14 justify-center items-center hover:bg-MUN-beigeBanner p-3">
               <svg width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path className=" group-hover:fill-MUN-bg-purple-dark" fillRule="evenodd" clipRule="evenodd" d="M15.9999 0.666504H18.6665V3.33317H21.3333H24H24V27.3332H24H21.3333H2.66667H0V24.6665V11.3332V8.6665V5.99984V3.33317H2.66667H5.33347V0.666504H8.00014V3.33317H15.9999V0.666504ZM5.33347 5.99984L2.66667 5.99984V8.6665H21.3333V5.99984L18.6665 5.99984H15.9999H8.00014H5.33347ZM2.66667 11.3332V24.6665H21.3333V11.3332H2.66667ZM5.33347 13.9998H8.00014V16.6665H5.33347V13.9998ZM13.3334 13.9998H10.6668V16.6665H13.3334V13.9998ZM15.9999 13.9998H18.6665V16.6665H15.9999V13.9998ZM8.00014 19.3332H5.33347V21.9998H8.00014V19.3332ZM10.6668 19.3332H13.3334V21.9998H10.6668V19.3332ZM18.6665 19.3332H15.9999V21.9998H18.6665V19.3332Z" fill="#53405F"/>
               </svg>
@@ -52,7 +52,7 @@ const Sidebar = () => {
         </button>
         <button>
           <Link href="/aboutUs">
-            <div className="group flex bg-MUN-sidebar-linen rounded-full w-16 h-16 justify-center items-center hover:bg-MUN-beigeBanner p-5">
+            <div className="group flex bg-MUN-sidebar-linen rounded-full w-14 h-14 justify-center items-center hover:bg-MUN-beigeBanner p-3">
             <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path className=" group-hover:fill-MUN-bg-purple-dark" fillRule="evenodd" clipRule="evenodd" d="M6.75 0.5H3.75V2H2.25V6.5L3.75 6.5V8H6.75V6.5H3.75V2H6.75V0.5ZM14.25 0.5H9.75V2H8.25V6.5L9.75 6.5V8H14.25V6.5L15.75 6.5V2H14.25V0.5ZM14.25 2V6.5H9.75V2H14.25ZM7.5 11V14H16.5V11H18V15.5H16.5H7.5H6V11H7.5ZM7.5 11V9.5H16.5V11H7.5ZM17.25 0.5H20.25V2H17.25V0.5ZM20.25 6.5V2H21.75V6.5L20.25 6.5ZM20.25 6.5V8H17.25V6.5H20.25ZM24 11H22.5V9.5H19.5V11H22.5V14H19.5V15.5H24V14V11ZM0 11H1.5V14H4.5V15.5H0V14V11ZM1.5 11V9.5H4.5V11H1.5Z" fill="#53405F"/>
             </svg>
--- app/home/page.js
+++ app/home/page.js
@@ -5,50 +5,55 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <>
+    <div className={`${styles.bgPaper}  min-h-screen`}>
         <Sidebar/>
-        <div className='layout flex'>
-        <div className="text-6xl space-y-5 pt-28 cursor-default">
-            <p>Boost your <span className="group text-white relative cursor-default before:bg-MUN-UCLAGold before:content-[''] before:absolute before:inset-0 before:transform before:scale-x-0 before:origin-right before:transition-transform before:duration-200 before:ease-in-out before:z-[-1] hover:before:scale-x-100 hover:before:origin-left hover:text-black">
-                public
-                </span>
-            </p>
-            <p>
-                <span className="text-white relative cursor-default before:bg-MUN-UCLAGold before:content-[''] before:absolute before:inset-0 before:transform before:scale-x-0 before:origin-right before:transition-transform before:duration-200 before:ease-in-out before:z-[-1] hover:before:scale-x-100 hover:before:origin-left hover:text-black">
-                speaking </span>skills in  <span className="text-white relative cursor-default before:bg-MUN-UCLAGold before:content-[''] before:absolute before:inset-0 before:transform before:scale-x-0 before:origin-right before:transition-transform before:duration-200 before:ease-in-out before:z-[-1] hover:before:scale-x-100 hover:before:origin-left hover:text-black">
-                Sepuluh
-                </span>
-            </p>
-            <p>
-                <span className="text-white relative cursor-default before:bg-MUN-UCLAGold before:content-[''] before:absolute before:inset-0 before:transform before:scale-x-0 before:origin-right before:transition-transform before:duration-200 before:ease-in-out before:z-[-1] hover:before:scale-x-100 hover:before:origin-left hover:text-black">
-                Nopember Institute of
-                </span>
-            </p>
-            <p>
-                <span className="text-white relative cursor-default before:bg-MUN-UCLAGold before:content-[''] before:absolute before:inset-0 before:transform before:scale-x-0 before:origin-right before:transition-transform before:duration-200 before:ease-in-out before:z-[-1] hover:before:scale-x-100 hover:before:origin-left hover:text-black">
-                Technology
-                </span>  leading
-            </p>
-            <p>MUN organization</p>   
-        </div>
-                    
-            <div className="h-[600px] w-[580px] relative mt-3">
-                <Image
-                    src='/heroHome.png'
-                    alt='hero home page'
-                    layout="fill" // required
-                    objectFit="cover" // change to suit your needs
-                    className="" // just an example
-                />
+        <div className='layout'>
+            <div className='hero flex flex-col h-full'>
+                <div className='hero__content flex'>
+                    <div className='content__main flex flex-col gap-9 pt-12'>
+                        <div className="content__text text-6xl space-y-5  cursor-default">
+                            <p className=' font-montserrat'>Boost your <span className={`${styles.span} font-spritz`}>
+                                Public
+                                </span>
+                            </p>
+                            <p className='font-montserrat'>
+                                <span className={`${styles.span} font-spritz`}>
+                                Speaking </span>skills in  <span className={`${styles.span} font-spritz`}>
+                                Sepuluh
+                                </span>
+                            </p>
+                            <p className={`${styles.span} font-spritz`}>Nopember Institute of</p>
+                            <p className='font-montserrat'><span className={`${styles.span} font-spritz`}>Technology</span> leading
+                            </p>
+                            <p className='font-montserrat'>MUN organization</p>   
+                        </div>     
+                        <button className="content__btn w-fit cursor-pointer font-montserrat font-ex my-1 bg-transparent border border-black rounded-full px-8 py-1 text-2xl text-black hover:bg-black hover:text-white" type="button">
+                            Register Here
+                        </button>
+                    </div>
+                    <div className="content__img h-[600px] w-[580px] relative mt-3">
+                        <Image
+                            src='/heroHome.png'
+                            alt='hero home page'
+                            layout="fill"
+                            objectFit="cover" 
+                            className="" 
+                        />
+                    </div>  
+                </div>
+                <div className='hero__side'>
+                    <div className='side__main flex justify-between'>
+                        <div className='side__blocks'>
+                            <div className=' bg-MUN-UCLAGold w-12 h-12 ml-12 animate-bounce'></div>
+                            <div className=' bg-MUN-solidPink w-12 h-12 animate-bounce'></div>    
+                        </div>
+                        <p className='side__text flex items-end font-montserrat'>ITS Model United Nations Club 2024</p>
+                    </div>
+                    <hr className='side__line border-black border-1'/>
+                </div>
             </div>
-            {/* <p>Boost your <span className={styles.span}>public </span></p>
-            <p><span className={styles.span}>speaking</span> skills in <span className={styles.span}>Sepuluh</span></p>
-            <p><span className={styles.span}>Nopember Institute of</span></p>
-            <p><span className={styles.span}>Technology</span> leading</p>
-            <p>MUN organization</p>   */}
         </div>
-        
-    </>
+    </div>
   )
 }
 
