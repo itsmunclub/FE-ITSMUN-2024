b3d6c12443c8662cc595d569669d304f7494581d
Dancingaroundthelies <its.azizaah@gmail.com>
2024-08-08 11:18:25 +0700

    starting footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-MUN-englishViolet py-[60px] px-[115px]">
+      <div className="containerr max-w-360 w-full h-full m-auto">
+        <div className="content flex flex-row h-full">
+          <img className="items-center" src="/PPfooter.svg" alt="Footer Logo"/>
+          <Link href="/home" className="">
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
