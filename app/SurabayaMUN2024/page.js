5affcd6c4dcd3c7a97e04acf5aac67850e1873bb
Dancingaroundthelies <its.azizaah@gmail.com>
2024-08-08 20:05:35 +0700

    creating page for sumun (not finished yet)

---
+++ app/SurabayaMUN2024/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './SurabayaMUN2024.module.css'
+
+const Sumun = () => {
+  return (
+    <div className={`${styles.bgHero}`}>
+        <p>testing</p>
+        <div className="h-[1280px] w-auto"></div>
+    </div>
+  )
+}
+
+export default Sumun
