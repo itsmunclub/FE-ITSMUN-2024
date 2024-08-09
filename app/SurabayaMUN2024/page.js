6c76cc83b45566911ebb021ef4d658553bd006ac
Dancingaroundthelies <its.azizaah@gmail.com>
2024-08-09 11:33:30 +0700

    fixing hero section in sumun page

--- app/SurabayaMUN2024/page.js
+++ app/SurabayaMUN2024/page.js
@@ -3,10 +3,20 @@ import styles from './SurabayaMUN2024.module.css'
 
 const Sumun = () => {
   return (
-    <div className={`${styles.bgHero}`}>
-        <p>testing</p>
-        <div className="h-[1280px] w-auto"></div>
-    </div>
+      <div className={`${styles.bgAll}`}>
+        <div className="bg-[url('/sumunHero.svg')] bg-cover lg:bg-auto bg-center bg-no-repeat hero flex flex-col md:pl-36 md:pr-12 sm:pl-14 pl-12 pr-4">
+          <div className="hero__content flex xl:h-[1280px] h-screen">
+            <div className="content__main flex flex-row">
+              <div className="content__text md:pt-[82px] md:pl-5 pt-[45px]">
+                <img className="h-auto w-auto" src="/heroosumun.svg" alt="hero sumun text"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="body">
+          <img className="" src="/bgBodySumun.svg" alt="bg-sumun"></img>
+        </div>
+      </div>
   )
 }
 
